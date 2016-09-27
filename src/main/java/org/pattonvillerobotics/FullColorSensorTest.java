1ff4e87b15895da2128550e4754dbde26ba05711
Joshua Zahner <joshuazahner@gmail.com>
2016-09-27 13:49:14 -0500

    Added Full Color Sensor Test OpMode
    This new opmode will allow for full testing of the BeaconColorSensor class including the .determineColor() method. It uses a color sensor and servo and sets the servo to different positions based on the color of the beacon.
    
    
    Former-commit-id: 9f613a55f2a0e78a6599771cebcf72218daf5a42

---
+++ src/main/java/org/pattonvillerobotics/FullColorSensorTest.java
@@ -0,0 +1,81 @@
+package org.pattonvillerobotics;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.pattonvillerobotics.commoncode.enums.AllianceColor;
+import org.pattonvillerobotics.commoncode.robotclasses.BeaconColorSensor;
+
+/**
+ * Created by developer on 9/27/16.
+ */
+
+@TeleOp(name = "Full Color Sensor Test", group = "Common")
+public class FullColorSensorTest extends LinearOpMode {
+
+    Servo buttonPresser;
+    BeaconColorSensor colorSensor;
+    AllianceColor allianceColor;
+
+    boolean aCurrState, aPrevState;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        initalize();
+        waitForStart();
+
+        while(opModeIsActive()){
+
+            aCurrState = gamepad1.a;
+
+            if(aCurrState && (aCurrState != aPrevState))  {
+                // button is transitioning to a pressed state. So Toggle LED
+                if(allianceColor == AllianceColor.BLUE){
+                    allianceColor = AllianceColor.RED;
+                }else{
+                    allianceColor = AllianceColor.BLUE;
+                }
+            }
+
+            aPrevState = aCurrState;
+
+            colorSensor.determineColor(allianceColor, new Runnable() {
+                @Override
+                public void run() {
+                    buttonPresser.setPosition(1.0);
+                }
+            }, new Runnable() {
+                @Override
+                public void run() {
+                    buttonPresser.setPosition(0.0);
+                }
+            }, new Runnable(){
+                @Override
+                public void run() {
+                    telemetry.addData("NONE", " ");
+                }
+            });
+
+            telemetry.addData("AllianceColor", allianceColor);
+            telemetry.update();
+            idle();
+
+        }
+
+    }
+
+
+    public void initalize(){
+        ColorSensor cs = hardwareMap.colorSensor.get("color_senor");
+
+        buttonPresser = hardwareMap.servo.get("servo");
+        buttonPresser.setPosition(0.5);
+
+        colorSensor = new BeaconColorSensor(cs);
+        allianceColor = AllianceColor.BLUE;
+    }
+
+}
