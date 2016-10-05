f0365ab6d88a518c38a026a16f8776817df3f437
Joshua Zahner <joshuazahner@gmail.com>
2016-10-04 19:42:26 -0500

    Updates to Common Autonomous
    Added more constants and changed naming scheme for some constant variables. Added two modules to be pseudocoded later.

--- src/main/java/org/pattonvillerobotics/robotclasses/CommonAutonomous.java
+++ src/main/java/org/pattonvillerobotics/robotclasses/CommonAutonomous.java
@@ -1,54 +1,87 @@
 package org.pattonvillerobotics.robotclasses;
 
+import org.pattonvillerobotics.commoncode.enums.AllianceColor;
+import org.pattonvillerobotics.commoncode.enums.Direction;
+
 /**
  * Created by developer on 10/4/16.
  */
 
 public class CommonAutonomous {
 
-    private static int WALL_TO_BALL_DISTANCE = 60;
-    private static int WALL_TO_TILE3_CENTER = 12;
-    private static int BALL_TO_TAPE2 = 60;
-    private static int TAPE_TO_BEACON = 20;
-    private static int TAPE1_TO_TAPE2 = 50;
-    private static int TILE3_CENTER_TO_TAPE = 60;
-    private static int BEACONCOLOR_TO_BEACONBUTTON = 5;
+    //Distance Constants (inches)
+    private static int ONE_TILE                         =   24;
+    private static int ROBOT_LENGTH                     =   18;
 
-    //Angle Constants
-    private static int FIRST_TURN = 53;
-    private static int SECOND_TURN = 37;
-    private static int THIRD_TURN = 90;
-    private static int FOURTH_TURN = 67;
-    private static int FIFTH_ANYMORE = 23;
+    private static int WALL_1_TO_BALL_DISTANCE          =   60;
+    private static int WALL_1_TO_TILE_3_CENTER          =   12;
+    private static int BALL_TO_TAPE_2                   =   60;
+    private static int TILE_3_CENTER_TO_TAPE_1          =   60;
+    private static int TILE_3_CENTER_TO_TAPE_2          =   92;
+    private static int TAPE_1_TO_TAPE_2                 =   48;
+    private static int TAPE_TO_BEACON                   =   24;
 
-    public static void hitBall() {
+    private static int BEACON_DISTANCE_BUFFER           =    5;
+    private static int BALL_DISPLACEMENT_BUFFER         =    2;
+
+    //Angle Constants (degrees)
+    private static int RIGHT_ANGLE                      =   90; //Right Angle
+    private static int Wall_1_CENTER_TO_TAPE_1_ANGLE    =   53; //Tile #3 -> Tape 1
+    private static int BALL_TO_TAPE_2_ANGLE             =   53; //Ball -> Tape 2
+    private static int TAPE_1__TO_STRAIGHT_ANGLE        =   37; //Tile #3 -> Tape 1 Straighten
+    private static int TAPE_2_TO_BALL_ANGLE             =   37; //Tape 2 -> Ball
+    private static int WALL_1_CENTER_TO_TAPE_2_ANGLE    =   67; //Tile #3 -> Tape 2
+    private static int TAPE_2_TO_STRAIGHT_ANGLE         =   23; //Tile #3 -> Tape 2 Straighten
+
+    private Direction turnDirection;
+
+    public CommonAutonomous(AllianceColor allianceColor){
+        if(allianceColor == AllianceColor.BLUE){
+            turnDirection = Direction.RIGHT;
+        }else{
+            turnDirection = Direction.LEFT;
+        }
+    }
+
+    //Autonomous Modules
+    public void wallPos1ToBall() {
         //Forward 60 inches
         //Hit Ball
     }
 
-    public static void hitBeacon() {
+    public void pressBeacon() {
+        //20 inches forward
         //read beacon color
         //rotate pressed mech
+        //4 inches forward
     }
 
-    public static void tile3ToBeacon1() {
+    public void wallPos1ToBeacon1() {
         //12 inches forward
-        //53 degrees right
+        //53 degrees turn
         //60 forward
-        //37 right
+        //37 turn
         //20 forward
     }
+    
+    public void wallPos1ToBeacon2() {
+        
+    }
 
-    public static void beacon1ToBeacon2() {
+    public void beacon1ToBeacon2() {
         //24 inches forward
-        //90 left
+        //90 turn
         //43 forward
-        //90 right
+        //90 turn
         //20 inches forward
     }
 
-    public static void backHitBall() {
+    public void tape1ToBall() {
         //60 inches back
+    }
+    
+    public void tape2ToBall() {
+        
     }
 
 }
