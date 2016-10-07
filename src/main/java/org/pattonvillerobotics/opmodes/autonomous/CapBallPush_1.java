e1309485af551df6c3be1b6f3c6ff2cbe85e0714
wrightk03 <kyle.wright02@gmail.com>
2016-10-06 19:59:49 -0500

    Test Autonomous

--- src/main/java/org/pattonvillerobotics/opmodes/autonomous/CapBallPush_1.java
+++ src/main/java/org/pattonvillerobotics/opmodes/autonomous/CapBallPush_1.java
@@ -1,7 +1,7 @@
 package org.pattonvillerobotics.opmodes.autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.pattonvillerobotics.commoncode.enums.Direction;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.SimpleDrive;
@@ -10,10 +10,10 @@ import org.pattonvillerobotics.commoncode.robotclasses.drive.SimpleDrive;
  * Created by mostafay on 10/4/16.
  */
 
+@Autonomous(name = "FiveSeconds", group = "None")
 public class CapBallPush_1 extends LinearOpMode {
 
     SimpleDrive drive;
-    HardwareMap hardwareMap;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -22,14 +22,23 @@ public class CapBallPush_1 extends LinearOpMode {
         //5 inches forward
         //Park on center vortex
 
-        drive.move(Direction.FORWARD, 0.5);
-        sleep(5000);
+        initialize();
+        waitForStart();
+
+        while(opModeIsActive()){
+            drive.move(Direction.FORWARD, 0.5);
+        }
+
+
 
     }
 
-    public void initalize(){
+    public void initialize(){
 
+        telemetry.addData("Startup", "started!");
         drive = new SimpleDrive(this, hardwareMap);
+        telemetry.addData("Drive", drive);
+        telemetry.update();
 
     }
 
