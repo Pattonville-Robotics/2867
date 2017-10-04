5358cd43b7ae4b637364339d8b8a32b2df6b332e
greg-bahr <greg.bahr123@gmail.com>
2017-10-04 09:26:54 -0500

    started mainteleop, wrote bengrabber mechanism
    Former-commit-id: fb7c0ef497bad3770825d2ddbea3cd922c41aff4

--- src/main/java/org/pattonvillerobotics/robotclasses/BenGrabber.java
+++ src/main/java/org/pattonvillerobotics/robotclasses/BenGrabber.java
@@ -1,4 +1,54 @@
 package org.pattonvillerobotics.robotclasses;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.apache.commons.math3.util.FastMath;
+
+import static org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive.TARGET_REACHED_THRESHOLD;
+
 public class BenGrabber {
+
+    //Fields
+    private DcMotor motor;
+    private LinearOpMode opMode;
+
+    //Constructor
+    public BenGrabber(HardwareMap hardwareMap, LinearOpMode opMode) {
+        motor = hardwareMap.dcMotor.get("motor");
+        this.opMode = opMode;
+    }
+
+    //Methods
+
+    public void open() {
+
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setTargetPosition(0);
+        motor.setPower(0.2);
+
+        while ((!reachedTarget(motor.getCurrentPosition(), motor.getTargetPosition()) || motor.isBusy()) && !opMode.isStopRequested() && opMode.opModeIsActive()) {
+            opMode.idle();
+        }
+
+        motor.setPower(0);
+    }
+
+    public void close() {
+
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setTargetPosition(120);
+        motor.setPower(0.2);
+
+        while (!reachedTarget(motor.getCurrentPosition(), motor.getTargetPosition()) && !opMode.isStopRequested() && opMode.opModeIsActive()) {
+            opMode.idle();
+        }
+
+        motor.setPower(0);
+    }
+
+    protected boolean reachedTarget(int currentPositionLeft, int targetPositionLeft) {
+        return FastMath.abs(currentPositionLeft - targetPositionLeft) < TARGET_REACHED_THRESHOLD;
+    }
 }
