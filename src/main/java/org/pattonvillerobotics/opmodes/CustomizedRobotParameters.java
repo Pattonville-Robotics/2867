a70f4c4f00a78c8795a04bc8c56fc7380faac00c
greg <greg.bahr123@gmail.com>
2017-10-08 00:00:01 -0500

    opmode to test opencv init and to save png from vuforia
    Former-commit-id: 8aaa7116d7c5d83197e83e7b0fb5da640d5d25d9

--- src/main/java/org/pattonvillerobotics/opmodes/CustomizedRobotParameters.java
+++ src/main/java/org/pattonvillerobotics/opmodes/CustomizedRobotParameters.java
@@ -1,6 +1,7 @@
 package org.pattonvillerobotics.opmodes;
 
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.teamcode.R;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.RobotParameters;
 import org.pattonvillerobotics.commoncode.robotclasses.vuforia.VuforiaParameters;
 
@@ -23,6 +24,7 @@ public class CustomizedRobotParameters {
 
         VUFORIA_PARAMETERS = new VuforiaParameters.Builder()
                                 .cameraDirection(VuforiaLocalizer.CameraDirection.BACK)
+                                .cameraMonitorViewId(R.id.cameraMonitorViewId)
                                 .licenseKey("AclLpHb/////AAAAGa41kVT84EtWtYJZW0bIHf9DHg5EHVYWCqExQMx6bbuBtjFeYdvzZLExJiXnT31qDi3WI3QQnOXH8pLZ4cmb39d1w0Oi7aCwy35ODjMvG5qX+e2+3v0l3r1hPpM8P7KPTkRPIl+CGYEBvoNkVbGGjalCW7N9eFDV/T5CN/RQvZjonX/uBPKkEd8ciqK8vWgfy9aPEipAoyr997DDagnMQJ0ajpwKn/SAfaVPA4osBZ5euFf07/3IUnpLEMdMKfoIH6QYLVgwbPuVtUiJWM6flzWaAw5IIhy0XXWwI0nGXrzVjPwZlN3El4Su73ADK36qqOax/pNxD4oYBrlpfYiaFaX0Q+BNro09weXQEoz/Mfgm")
                                 .build();
     }
