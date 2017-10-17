f9aa8fa574a3c2e79102b66d6840f4495537d6ec
William Skaggs <wskaggs3262@gmail.com>
2017-10-16 19:30:07 -0500

    Last commit before project restructure

--- src/main/java/org/pattonvillerobotics/opmodes/test/ServoArmTest.java
+++ src/main/java/org/pattonvillerobotics/opmodes/test/ServoArmTest.java
@@ -3,6 +3,8 @@ package org.pattonvillerobotics.opmodes.test;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.pattonvillerobotics.commoncode.enums.ColorSensorColor;
+import org.pattonvillerobotics.commoncode.opmodes.OpModeGroups;
 import org.pattonvillerobotics.commoncode.robotclasses.gamepad.GamepadData;
 import org.pattonvillerobotics.commoncode.robotclasses.gamepad.ListenableButton;
 import org.pattonvillerobotics.commoncode.robotclasses.gamepad.ListenableGamepad;
@@ -11,7 +13,7 @@ import org.pattonvillerobotics.robotclasses.ServoArm;
 /**
  * Created by skaggsw on 10/5/17.
  */
-@TeleOp(name="Servo Arm Test")
+@TeleOp(name="ServoArmTest", group= OpModeGroups.TESTING)
 public class ServoArmTest extends LinearOpMode {
 
     private ServoArm servoArm;
@@ -26,14 +28,11 @@ public class ServoArmTest extends LinearOpMode {
         gamepad.getButton(GamepadData.Button.X).addListener(ListenableButton.ButtonState.JUST_PRESSED, new ListenableButton.ButtonListener() {
             @Override
             public void run() {
-                servoArm.extendArm();
-            }
-        });
-
-        gamepad.getButton(GamepadData.Button.Y).addListener(ListenableButton.ButtonState.JUST_PRESSED, new ListenableButton.ButtonListener() {
-            @Override
-            public void run() {
-                servoArm.retractArm();
+                if(servoArm.ballColor == ColorSensorColor.BLUE) {
+                    servoArm.extendArm();
+                } else if(servoArm.ballColor == ColorSensorColor.RED) {
+                    servoArm.retractArm();
+                }
             }
         });
 
@@ -44,6 +43,7 @@ public class ServoArmTest extends LinearOpMode {
             telemetry.addData("Ball Color:", servoArm.ballColor);
             telemetry.addData("Servo Position:", servoArm.getServoPosition());
             telemetry.update();
+            gamepad.update(new GamepadData(gamepad1));
         }
     }
 }
