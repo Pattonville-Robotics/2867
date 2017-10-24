7d125f4cfe47d9abda38ac2c4bdb29aedde74af9
Greg Bahr <greg.bahr123@gmail.com>
2017-10-24 15:31:20 -0500

    removed robot class

--- TeamCode/src/main/java/org/pattonvillerobotics/opmodes/test/GrabberTest.java
+++ TeamCode/src/main/java/org/pattonvillerobotics/opmodes/test/GrabberTest.java
@@ -7,23 +7,23 @@ import org.pattonvillerobotics.commoncode.opmodes.OpModeGroups;
 import org.pattonvillerobotics.commoncode.robotclasses.gamepad.GamepadData;
 import org.pattonvillerobotics.commoncode.robotclasses.gamepad.ListenableButton;
 import org.pattonvillerobotics.commoncode.robotclasses.gamepad.ListenableGamepad;
-import org.pattonvillerobotics.robotclasses.Robot;
+import org.pattonvillerobotics.robotclasses.mechanisms.BenClaw;
 
 @TeleOp(name = "GrabberTest", group = OpModeGroups.TESTING)
 public class GrabberTest extends LinearOpMode {
 
-    private Robot robot;
+    private BenClaw claw;
     private ListenableGamepad gamepad;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        robot = new Robot(hardwareMap,this);
+        claw = new BenClaw(hardwareMap, this);
         gamepad = new ListenableGamepad();
 
         gamepad.getButton(GamepadData.Button.X).addListener(ListenableButton.ButtonState.JUST_PRESSED, new ListenableButton.ButtonListener() {
             @Override
             public void run() {
-                robot.claw.togglePosition();
+                claw.togglePosition();
             }
         });
 
@@ -31,7 +31,7 @@ public class GrabberTest extends LinearOpMode {
 
         while(opModeIsActive()) {
             gamepad.update(new GamepadData(gamepad1));
-            telemetry.addData("Servo Position: ", robot.claw.getServoPosition());
+            telemetry.addData("Servo Position: ", claw.getServoPosition());
             //slideMotor.setPower(gamepad1.right_trigger-gamepad1.left_trigger);
             telemetry.update();
         }
