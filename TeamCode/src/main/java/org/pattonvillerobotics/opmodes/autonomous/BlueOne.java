63144d87c3cf80efb688ee56bb861c39649119c7
William Skaggs <wskaggs3262@gmail.com>
2017-11-07 16:12:51 -0600

    Worked on autonomous, adding stuff for OpenCV color detection, also updated ServoArmTest

--- TeamCode/src/main/java/org/pattonvillerobotics/opmodes/autonomous/BlueOne.java
+++ TeamCode/src/main/java/org/pattonvillerobotics/opmodes/autonomous/BlueOne.java
@@ -3,27 +3,41 @@ package org.pattonvillerobotics.opmodes.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.pattonvillerobotics.commoncode.enums.ColorSensorColor;
 import org.pattonvillerobotics.commoncode.enums.Direction;
 import org.pattonvillerobotics.commoncode.opmodes.OpModeGroups;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.MecanumEncoderDrive;
+import org.pattonvillerobotics.commoncode.robotclasses.opencv.JewelColorDetector;
+import org.pattonvillerobotics.commoncode.robotclasses.opencv.util.PhoneOrientation;
 import org.pattonvillerobotics.commoncode.robotclasses.vuforia.VuforiaNavigation;
+import org.pattonvillerobotics.commoncode.robotclasses.vuforia.VuforiaParameters;
 import org.pattonvillerobotics.opmodes.CustomizedRobotParameters;
 import org.pattonvillerobotics.robotclasses.mechanisms.BenClaw;
+import org.pattonvillerobotics.robotclasses.mechanisms.ServoArm;
 
 
 @Autonomous(name = "Blue 1", group = OpModeGroups.MAIN)
 public class BlueOne extends LinearOpMode {
 
     private MecanumEncoderDrive drive;
-    //private ServoArm arm;
+    private ServoArm arm;
     private BenClaw claw;
+    private JewelColorDetector jewelColorDetector;
     private VuforiaNavigation vuforia;
+
+    private VuforiaParameters VUFORIA_PARAMETERS = new VuforiaParameters.Builder()
+            .phoneLocation(0, 0, 0, AxesOrder.XYZ, 90, -90, 0)
+            .cameraDirection(VuforiaLocalizer.CameraDirection.BACK)
+            .licenseKey("AclLpHb/////AAAAGa41kVT84EtWtYJZW0bIHf9DHg5EHVYWCqExQMx6bbuBtjFeYdvzZLExJiXnT31qDi3WI3QQnOXH8pLZ4cmb39d1w0Oi7aCwy35ODjMvG5qX+e2+3v0l3r1hPpM8P7KPTkRPIl+CGYEBvoNkVbGGjalCW7N9eFDV/T5CN/RQvZjonX/uBPKkEd8ciqK8vWgfy9aPEipAoyr997DDagnMQJ0ajpwKn/SAfaVPA4osBZ5euFf07/3IUnpLEMdMKfoIH6QYLVgwbPuVtUiJWM6flzWaAw5IIhy0XXWwI0nGXrzVjPwZlN3El4Su73ADK36qqOax/pNxD4oYBrlpfYiaFaX0Q+BNro09weXQEoz/Mfgm")
+            .build();
 
     @Override
     public void runOpMode() throws InterruptedException {
         initialize();
+        JewelColorDetector.Analysis analysis;
         vuforia.activateTracking();
 
         RelicRecoveryVuMark columnKey;
@@ -43,6 +57,33 @@ public class BlueOne extends LinearOpMode {
         telemetry.addData("Column Key: ", columnKey).setRetained(true);
         telemetry.update();
 
+        jewelColorDetector.process(vuforia.getImage());
+        analysis = jewelColorDetector.getAnalysis();
+        while(analysis.leftJewelColor == null && analysis.rightJewelColor == null) {
+            jewelColorDetector.process(vuforia.getImage());
+            analysis = jewelColorDetector.getAnalysis();
+        }
+
+
+        switch (analysis.leftJewelColor) {
+            case RED:
+                drive.moveInches(Direction.BACKWARD,2,0.5);
+                break;
+            case BLUE:
+                drive.moveInches(Direction.FORWARD,2,0.5);
+                break;
+            default:
+                switch (analysis.rightJewelColor) {
+                    case RED:
+                        drive.moveInches(Direction.FORWARD,2,0.5);
+                        break;
+                    case BLUE:
+                        drive.moveInches(Direction.BACKWARD,2,0.5);
+                        break;
+                    default:
+                        break;
+                }
+        }
         //arm.extendArm();
 
     /*    jewelColor = arm.senseBallColor();
@@ -91,8 +132,9 @@ public class BlueOne extends LinearOpMode {
 
     public void initialize() {
         drive = new MecanumEncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS);
-        //arm = new ServoArm(hardwareMap, this);
+        arm = new ServoArm(hardwareMap, this);
         claw = new BenClaw(hardwareMap, this);
+        jewelColorDetector = new JewelColorDetector(PhoneOrientation.PORTRAIT_INVERSE);
         vuforia = new VuforiaNavigation(CustomizedRobotParameters.VUFORIA_PARAMETERS);
     }
 }
--- TeamCode/src/main/java/org/pattonvillerobotics/robotclasses/mechanisms/ServoArm.java
+++ TeamCode/src/main/java/org/pattonvillerobotics/robotclasses/mechanisms/ServoArm.java
@@ -5,8 +5,6 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.pattonvillerobotics.commoncode.enums.ColorSensorColor;
-
 /**
  * Created by skaggsw on 10/5/17.
  * <p>
@@ -15,9 +13,6 @@ import org.pattonvillerobotics.commoncode.enums.ColorSensorColor;
  */
 public class ServoArm extends AbstractMechanism {
 
-    final double SCALE_FACTOR = 255;
-    //private DistanceSensor sensorDistance;
-    public ColorSensorColor ballColor;
     private Servo servo;
     private ColorSensor sensorColor;
 
@@ -30,22 +25,20 @@ public class ServoArm extends AbstractMechanism {
     public ServoArm(HardwareMap hardwareMap, LinearOpMode linearOpMode) {
         super(hardwareMap, linearOpMode);
         servo = hardwareMap.servo.get("servo_arm");
-        sensorColor = hardwareMap.get(ColorSensor.class, "sensor_color_distance");
-        //sensorDistance = hardwareMap.get(DistanceSensor.class,"sensor_color_distance");
     }
 
     /**
      * Extends the claw arm
      */
     public void extendArm() {
-        servo.setPosition(1);
+        servo.setPosition(0);
     }
 
     /**
      * Retracts the claw arm
      */
     public void retractArm() {
-        servo.setPosition(0.15);
+        servo.setPosition(0.6);
     }
 
     /**
@@ -54,27 +47,5 @@ public class ServoArm extends AbstractMechanism {
      */
     public double getServoPosition() {
         return servo.getPosition();
-    }
-
-    /**
-     * Uses the red and blue values from the color sensor to change, then return an enum of the color,
-     * either red, blue, or green if it is not clearly one or the other
-     * @return an enum of the color sensed by the color sensor
-     */
-    public ColorSensorColor senseBallColor() {
-        if(sensorColor.red()*SCALE_FACTOR > 20 || sensorColor.blue()*SCALE_FACTOR > 20) {
-            ballColor = sensorColor.red() > sensorColor.blue() ? ColorSensorColor.RED : ColorSensorColor.BLUE;
-        } else {
-            ballColor = ColorSensorColor.GREEN;
-        }
-        return ballColor;
-    }
-
-    /**
-     * Returns the last sensed ball color
-     * @return an enum of the last sensed ball color
-     */
-    public ColorSensorColor getBallColor() {
-        return ballColor;
     }
 }
