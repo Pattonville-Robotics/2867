a07fd831472b9731bb17af15da60a7410d12ff18
Adedeji Coker <cokera01@student.psdr3.org>
2018-11-01 18:56:09 -0500

    Third Autonomous, but for a different quadrant

--- TeamCode/src/main/java/org/pattonvillerobotics/robotclasses/IntakeExtenderMechanism.java
+++ TeamCode/src/main/java/org/pattonvillerobotics/robotclasses/IntakeExtenderMechanism.java
@@ -7,30 +7,43 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class IntakeExtenderMechanism extends AbstractMechanism {
 
     public DcMotor spinMotor;
+    public DcMotor leftExtendMotor;
+    public DcMotor rightExtendMotor;
 
     public IntakeExtenderMechanism(HardwareMap hardwareMap, LinearOpMode linearOpMode) {
         super(hardwareMap,linearOpMode);
         spinMotor = hardwareMap.dcMotor.get("spin_motor");
-        extendMotor1 = hardwareMap.dcMotor.get("extend_motor_one");
-        extendMotor2 = hardwareMap.dcMotor.get("extend_motor_two");
+        leftExtendMotor = hardwareMap.dcMotor.get("left_extend_motor");
+        rightExtendMotor = hardwareMap.dcMotor.get("right_extend_motor");
     }
 
     public void extendIntake() {
-        spinMotor.setTargetPosition(spinMotor.getCurrentPosition()-(1440*6));
-        while(Math.abs(spinMotor.getCurrentPosition()-spinMotor.getTargetPosition()) > 16) {
-            spinMotor.setPower(-0.7);
+        leftExtendMotor.setTargetPosition(leftExtendMotor.getCurrentPosition()-(360));
+        while(Math.abs(leftExtendMotor.getCurrentPosition()-leftExtendMotor.getTargetPosition()) > 16) {
+            leftExtendMotor.setPower(-0.7);
         }
-        spinMotor.setPower(0);
+        leftExtendMotor.setPower(0);
+        rightExtendMotor.setTargetPosition(rightExtendMotor.getCurrentPosition()+(360));
+        while(Math.abs(rightExtendMotor.getCurrentPosition()-rightExtendMotor.getTargetPosition()) > 16) {
+            rightExtendMotor.setPower(0.7);
+        }
+        rightExtendMotor.setPower(0);
         spinMotor.setPower(0.4);
 
     }
 
 
     public void recallIntake() {
-        spinMotor.setTargetPosition(spinMotor.getCurrentPosition()-(1440*6));
-        while(Math.abs(spinMotor.getCurrentPosition()-spinMotor.getTargetPosition()) > 16) {
-            spinMotor.setPower(-0.7);
+        rightExtendMotor.setTargetPosition(rightExtendMotor.getCurrentPosition()-(360));
+        while(Math.abs(rightExtendMotor.getCurrentPosition()-rightExtendMotor.getTargetPosition()) > 16) {
+            rightExtendMotor.setPower(-0.7);
         }
+        rightExtendMotor.setPower(0);
+        leftExtendMotor.setTargetPosition(leftExtendMotor.getCurrentPosition()+(360));
+        while(Math.abs(leftExtendMotor.getCurrentPosition()-leftExtendMotor.getTargetPosition()) > 16) {
+            leftExtendMotor.setPower(0.7);
+        }
+        leftExtendMotor.setPower(0);
         spinMotor.setPower(0);
     }
 
